1a1648405c65be779fad1843751b0ecbc77716c6
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-01-08 12:11:42 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,31 +6,38 @@ import TextForm from './components/TextForm';
 
 
 function App() {
-  const  [darkMode, setdarkMode] = useState('light');
+  const  [mode, setMode] = useState('light');
+  //whether dark mode is enabled or not
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+
+
+
+
 
 
 
 
 
   return (
-    <>
-      <Navbar Title = "TextUtils" aboutText="About"  mode={darkMode}    />
-      {/* <Navbar /> */}
-
-      <div className="container">
-        
-      <TextForm headings = "Enter The Text for analyze Below" />
-
-      {/* <About /> */}
-
-
-      </div>
-
-
-
-
-
-    </>
+   <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    </div>
+    </> 
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -68,16 +68,11 @@ export default function TextForm(props) {
 
   return (
     <>
-      <div className="container">
-        <h1>{props.headings}</h1>
+     <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
+
+        <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea
-            className="form-control"
-            value={text}
-            onChange={handleOnChange}
-            id="exampleFormControlTextarea1"
-            rows="8"
-          ></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
         </div>
 
         <button className="btn btn-primary mx-1" onClick={btn_call}>
@@ -130,14 +125,14 @@ export default function TextForm(props) {
         </div> */}
       </div>
 
-      <div className="container my-3">
+      <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your Text Summary</h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes Read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
       </div>
     </>
   );
