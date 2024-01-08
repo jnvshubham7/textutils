11cb6c2a81bddedad7f4d18f39edb5a8b4dbd4bf
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-01-09 00:54:14 +0530

    final chnages for depoly in github

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,31 @@ import PropTypes from 'prop-types'
 
 
 export default function Navbar(props) {
+
+
+  const handleTitleClick = (e) => {
+    e.preventDefault();
+    //not to reload the page
+    
+  };
+
+  const handleHomeClick = (e) => {
+    e.preventDefault();
+    // Add any custom behavior you want when the "Home" link is clicked (if needed)
+  };
+
+
+
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a className="navbar-brand" href="#" onClick={handleTitleClick}>{props.title}</a>      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#" onClick={handleHomeClick}>Home</a>
           </li>
           {/* <li className="nav-item">
             <Link className="nav-link"  to="/about">{"About"}</Link>
